e7417bb23f5852aa36e8ea12f917226821f134f4
Glitch (glitch-hello-website) <none>
2022-04-21 04:13:37 +0000

    🗾🍇 Checkpoint ./index.html:44064265/144 ./script.js:44064265/336

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 //first typing animation
-var typed = new Typed(".typing",{
-    strings:["WEBDEVSKI", "WEBDEVSKI", "WEBDEVSKI"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
+var typed = new Typed(".typing", {
+  strings: ["WEBDEVSKI", "WEBDEVSKI", "WEBDEVSKI"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
 });
-
