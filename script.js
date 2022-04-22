81a007b37029e45dcf143115d4b3583b14a23083
Glitch (glitch-hello-website) <none>
2022-04-22 02:41:21 +0000

    💗👐 Checkpoint ./style.css:44064265/586 ./script.js:44064265/240

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }
+        else{
+            $('.navbar').removeClass("sticky");
+        }
+    })
+});
 
 
 
 //first typing animation
-var typed = new Typed(".typing", {
+/*var typed = new Typed(".typing", {
   strings: ["WEBDEVSKI", "WEBDEVSKI", "WEBDEVSKI"],
   typeSpeed: 100,
   backSpeed: 60,
   loop: true,
-});
+});*/
