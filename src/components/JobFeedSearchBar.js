40568405c4cab59aaa770d813fa372d737d62dc5
YevheniyaSolomyana <72937387+YevheniyaSolomyana@users.noreply.github.com>
2023-05-08 20:16:09 -0400

    Updating JobFeed

--- src/components/JobFeedSearchBar.js
+++ src/components/JobFeedSearchBar.js
@@ -3,10 +3,26 @@ import { Form, Col } from "react-bootstrap";
 
 const JobFeedSearchBar = ({ params, onParamChange }) => {
     return (
-        <>
-            <Form.Label>Description</Form.Label>
-            <Form.Control onChange={onParamChange} value={params.description} name="description" type="text" />
-        </>
+        <Form className="mb-4">
+            <Form.Group as={Col}>
+                <Form.Label>Description</Form.Label>
+                <Form.Control placeholder="Search for details in the description..." 
+                onChange={onParamChange} value={params.description} 
+                name="description" type="text" />
+            </Form.Group>
+
+            <Form.Group as={Col}>
+                <Form.Label>Location</Form.Label>
+                <Form.Control placeholder="Search for location..." 
+                onChange={onParamChange} value={params.location} 
+                name="location" type="text" />
+            </Form.Group>
+
+            <Form.Group as={Col} xs="auto" className="ml-2">
+                <Form.Check onChange={onParamChange} value={params.type} name="type" 
+                id="type" label="Only Full-Time" type="checkbox" />
+            </Form.Group>
+        </Form>
     )
 }
  
